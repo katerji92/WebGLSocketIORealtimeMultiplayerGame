8124d5bc405948a73728c5a9570ea457def0077e
Mostafa Katerji <mostafakaterji@gmail.com>
2015-10-25 21:12:49 +0200

    Added two player bars, one of which can be controlled with mouse clicks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,27 @@
+/// <reference path="babylon.js" />
+var player1Bar;
+var player2Bar;
+
+var scene;
 document.addEventListener("DOMContentLoaded", function(event) {
-	
-  console.log("Hello BabylonJS");
   
   // Get the canvas element from our HTML above
   var canvas = document.querySelector("#render");
 
   // Load the BABYLON 3D engine
   var engine = new BABYLON.Engine(canvas, true);
-	
-	// This begins the creation of a function that we will 'call' just after it's built
+  
+  // This begins the creation of a function that we will 'call' just after it's built
   var createScene = function () {
 
     // Now create a basic Babylon Scene object 
-    var scene = new BABYLON.Scene(engine);
+    scene = new BABYLON.Scene(engine);
 
     // Change the scene background color to black.
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
     // This creates and positions a free camera
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    var camera = new BABYLON.TargetCamera("camera1", new BABYLON.Vector3(0, 0, -10), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -27,20 +30,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     camera.attachControl(canvas, false);
 
     // This creates a light, aiming 0,1,0 - to the sky.
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 4, -12), scene);
 
     // Dim the light a small amount
     light.intensity = .5;
 
+    function createPlayerBar(startingXPosition){
+      var bar = BABYLON.Mesh.CreateCylinder("cylinder", 4, 1, 1, 20, 1, scene, false);
+      bar.position.x = startingXPosition;
+      bar.checkCollisions = true;
+      return bar;
+    }
     // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    player1Bar = createPlayerBar(-4);
+    player2Bar = createPlayerBar(4);
 
     // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+    //sphere.position.y = 1;
 
     // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    var wall = BABYLON.Mesh.CreatePlane("wall", 14.0, scene);
+    wall.position.z = 5;
+    wall.material = new BABYLON.StandardMaterial("wallMat", scene);
+    wall.material.emissiveColor = new BABYLON.Color3(.1, .05, .3);
 
+    scene.collisionsEnabled = true;
+    
     // Leave this function
     return scene;
 
@@ -55,5 +70,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   window.addEventListener("resize", function () {
     engine.resize();
   });
-	
+
+  //When pointer down event is raised
+    scene.onPointerDown = function (evt, pickResult) {
+        // if the click hits the ground object, we change the impact position
+        if (pickResult.hit) {
+            player1Bar.position.y = pickResult.pickedPoint.y;
+        }
+    };
+
+
 });
